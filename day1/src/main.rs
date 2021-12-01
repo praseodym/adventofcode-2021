10decdc3385012253488ee57d65b3d163a3e0dc9
Mark Janssen <mark@praseodym.net>
2021-12-01 22:22:48 +0100

    Day 1: use array_windows

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,46 +1,16 @@
 #![feature(test)]
+#![feature(array_windows)]
 
 extern crate test;
 
-use std::collections::VecDeque;
 use std::error::Error;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use test::Bencher;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file = File::open("input")?;
-    let reader = BufReader::new(file);
-
-    let mut previous_measurement = None;
-    let mut increments_1 = 0;
-    let mut increments_2 = 0;
-    let mut sliding_window = VecDeque::new();
-
-    for (_, line) in reader.lines().enumerate() {
-        let new_measurement = line?.parse::<i32>()?;
-
-        // First strategy
-        if let Some(d) = previous_measurement {
-            if new_measurement > d {
-                increments_1 += 1;
-            }
-        }
-        previous_measurement = Some(new_measurement);
-
-        // Second strategy with sliding window
-        if sliding_window.len() == 3 {
-            let first_sum: i32 = sliding_window.iter().sum();
-            sliding_window.pop_front();
-            sliding_window.push_back(new_measurement);
-            let second_sum: i32 = sliding_window.iter().sum();
-            if second_sum > first_sum {
-                increments_2 += 1;
-            }
-        } else {
-            sliding_window.push_back(new_measurement);
-        };
-    }
+    let measurements = read_measurements();
+    let increments_1 = first(&measurements);
+    let increments_2 = second(&measurements);
 
     assert_eq!(increments_1, 1167);
     assert_eq!(increments_2, 1130);
@@ -50,7 +20,53 @@ fn main() -> Result<(), Box<dyn Error>> {
     Result::Ok(())
 }
 
-#[bench]
-fn bench(b: &mut Bencher) {
-    b.iter(main);
+fn read_measurements() -> Vec<i32> {
+    let file = File::open("input").unwrap();
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map(|line_str| line_str.unwrap().parse::<i32>().unwrap())
+        .collect()
+}
+
+fn first(measurements: &[i32]) -> i32 {
+    measurements
+        .array_windows()
+        .map(|&[a, b]| if b > a { 1 } else { 0 })
+        .sum::<i32>()
+}
+
+fn second(measurements: &[i32]) -> i32 {
+    measurements
+        .array_windows::<3>()
+        .map(|w| w.iter().sum())
+        .collect::<Vec<i32>>()
+        .array_windows()
+        .map(|&[a, b]| if b > a { 1 } else { 0 })
+        .sum::<i32>()
+}
+
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+
+    use super::*;
+
+    #[bench]
+    fn bench_main(b: &mut Bencher) {
+        b.iter(main);
+    }
+
+    #[bench]
+    fn bench_first(b: &mut Bencher) {
+        let measurements = read_measurements();
+        b.iter(|| first(&measurements));
+    }
+
+    #[bench]
+    fn bench_second(b: &mut Bencher) {
+        let measurements = read_measurements();
+        b.iter(|| second(&measurements));
+    }
 }
