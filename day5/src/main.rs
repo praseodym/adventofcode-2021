b16ba196d5b0324173bd3609f07658b59252433d
Mark Janssen <mark@praseodym.net>
2021-12-05 20:56:48 +0000

    Day 5: part two

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -6,7 +6,8 @@ fn main() {
     let file = File::open("input").unwrap();
     let reader = BufReader::new(file);
 
-    let mut grid = [[0u8; 1000]; 1000];
+    let mut grid1 = [[0u8; 1000]; 1000];
+    let mut grid2 = [[0u8; 1000]; 1000];
 
     for (_, line) in reader.lines().enumerate() {
         let line = line.unwrap();
@@ -16,47 +17,76 @@ fn main() {
             .flat_map(|s| s.split(',').map(|n| n.parse::<usize>().unwrap()))
             .collect();
 
-        let x1: usize;
-        let y1: usize;
-        let x2: usize;
-        let y2: usize;
-
-        if coords[0] <= coords[2] {
-            x1 = coords[0];
-            x2 = coords[2];
-        } else {
-            x1 = coords[2];
-            x2 = coords[0];
-        }
-        if coords[1] <= coords[3] {
-            y1 = coords[1];
-            y2 = coords[3];
-        } else {
-            y1 = coords[3];
-            y2 = coords[1];
-        }
+        let x1 = coords[0];
+        let y1 = coords[1];
+        let x2 = coords[2];
+        let y2 = coords[3];
 
         // println!("coords: {},{} -> {},{}", x1, y1, x2, y2);
 
-        // mark points in grid, considering only lines (x1 == x2 || y1 == y2) for now
-        if x1 == x2 {
-            for y in y1..y2+1 {
-                // println!("Y-line mark point {},{}", x1, y);
-                grid[y][x1] += 1;
+        // mark points in grid, considering only lines (x1 == x2 || y1 == y2) for grid1
+        if x1 == x2 && y1 <= y2 {
+            for y in y1..y2 + 1 {
+                grid1[y][x1] += 1;
+                grid2[y][x1] += 1;
             }
-        } else if y1 == y2 {
-            for x in x1..x2+1 {
-                // println!("X-line mark point {},{}", x, y1);
-                grid[y1][x] += 1;
+        } else if x1 == x2 && y1 > y2 {
+            for y in y2..y1 + 1 {
+                grid1[y][x1] += 1;
+                grid2[y][x1] += 1;
+            }
+        } else if y1 == y2 && x1 <= x2 {
+            for x in x1..x2 + 1 {
+                grid1[y1][x] += 1;
+                grid2[y1][x] += 1;
+            }
+        } else if y1 == y2 && x1 > x2 {
+            for x in x2..x1 + 1 {
+                grid1[y1][x] += 1;
+                grid2[y1][x] += 1;
+            }
+        } else if x1 <= x2 && y1 <= y2 {
+            let mut x = x1;
+            for y in y1..y2 + 1 {
+                grid2[y][x] += 1;
+                x += 1;
+            }
+        } else if x1 > x2 && y1 <= y2 {
+            let mut x = x1 + 1;
+            for y in y1..y2 + 1 {
+                x -= 1;
+                grid2[y][x] += 1;
+            }
+        } else if x1 <= x2 && y1 > y2 {
+            let mut x = x1 - 1;
+            let mut y = y1 + 1;
+            loop {
+                x += 1;
+                y -= 1;
+                grid2[y][x] += 1;
+                if x == x2 {
+                    break;
+                }
+            }
+        } else if x1 > x2 && y1 > y2 {
+            let mut x = x1 + 1;
+            let mut y = y1 + 1;
+            loop {
+                x -= 1;
+                y -= 1;
+                grid2[y][x] += 1;
+                if x == x2 {
+                    break;
+                }
             }
         } else {
-            continue;
+            println!("wut? {},{} -> {},{}", x1, x2, y1, y2);
         }
     }
 
     // for y in 0..10 {
     //     for x in 0..10 {
-    //         let cnt = grid[y][x];
+    //         let cnt = grid2[y][x];
     //         if cnt == 0 {
     //             print!(".");
     //         } else {
@@ -67,12 +97,19 @@ fn main() {
     // }
 
     let mut answer1 = 0;
+    let mut answer2 = 0;
     for x in 0..1000 {
         for y in 0..1000 {
-            if grid[x][y] >= 2 {
+            if grid1[x][y] >= 2 {
                 answer1 += 1;
+            }
+            if grid2[x][y] >= 2 {
+                answer2 += 1;
             }
         }
     }
+    assert_eq!(answer1, 5698);
+    assert_eq!(answer2, 15463);
     println!("answer 1: {}", answer1);
+    println!("answer 2: {}", answer2);
 }
