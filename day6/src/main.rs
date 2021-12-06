f61e4e27f15932e9f3811b53b94f241b9fcb51cb
Mark Janssen <mark@praseodym.net>
2021-12-06 12:17:02 +0100

    Day 6: part two

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -5,26 +5,30 @@ fn main() {
     let file = File::open("input").unwrap();
     let mut reader = BufReader::new(file);
 
+    type Fishies = [u64; 9];
+    let mut fishies: Fishies = Default::default();
+
     let mut buf: String = String::new();
     reader.read_line(&mut buf).unwrap();
-    let mut fishies: Vec<u8> = buf
-        .trim_end()
+    buf.trim_end()
         .split(',')
-        .map(|n| n.parse::<u8>().unwrap())
-        .collect();
+        .map(|n| n.parse::<usize>().unwrap())
+        .for_each(|f| fishies[f] += 1);
 
-    for _day in 0..80 {
-        let mut spawned = Vec::<u8>::new();
-        for fish in &mut fishies {
-            if *fish == 0 {
-                spawned.push(8);
-                *fish = 6;
-            } else {
-                *fish -= 1;
-            }
+    for day in 1..257 {
+        let mut new_fishies: Fishies = Default::default();
+        // spawn new fishies..
+        new_fishies[8] = fishies[0];
+        // .. and reset their times
+        new_fishies[6] = fishies[0];
+        // decrement timer for all other fishies
+        for i in 1..9 {
+            new_fishies[i - 1] += fishies[i];
         }
-        fishies.append(&mut spawned);
-        // println!("fishies on day {}: {:?}", _day, fishies);
+        fishies = new_fishies;
+        if day == 80 || day == 256 {
+            let sum: u64 = fishies.iter().sum();
+            println!("fishies on day {}: {}", day, sum);
+        }
     }
-    println!("fishies: {}", fishies.len());
 }
