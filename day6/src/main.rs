9aa5511de038e5c0e863db6ad406ed5c8d225957
Mark Janssen <mark@praseodym.net>
2021-12-06 15:00:52 +0100

    Day 6: cleanup

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -15,19 +15,24 @@ fn main() {
         .map(|n| n.parse::<usize>().unwrap())
         .for_each(|f| fishies[f] += 1);
 
-    for day in 1..257 {
+    for day in 1..=256 {
         let mut new_fishies: Fishies = Default::default();
         // spawn new fishies..
         new_fishies[8] = fishies[0];
-        // .. and reset their times
+        // .. and reset their timers
         new_fishies[6] = fishies[0];
         // decrement timer for all other fishies
-        for i in 1..9 {
+        for i in 1..=8 {
             new_fishies[i - 1] += fishies[i];
         }
         fishies = new_fishies;
         if day == 80 || day == 256 {
             let sum: u64 = fishies.iter().sum();
+            if day == 80 {
+                assert_eq!(sum, 352195);
+            } else if day == 256 {
+                assert_eq!(sum, 1600306001288);
+            }
             println!("fishies on day {}: {}", day, sum);
         }
     }
