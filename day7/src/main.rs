f845ab28af3a45184e7e781699a390ac8543c87e
Mark Janssen <mark@praseodym.net>
2021-12-07 13:54:56 +0100

    Day 7: part two

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::time::Instant;
 
 fn main() {
@@ -9,26 +10,32 @@ fn main() {
         .map(|n| n.parse::<i32>().unwrap())
         .collect();
 
-    let mut cheapest_fuel: Option<i32> = None;
+    let mut cheapest_fuel_part1 = 0i32;
+    let mut cheapest_fuel_part2 = 0i32;
 
     for target_position in 0..1000 {
-        let mut fuel = 0i32;
+        let mut fuel_part1 = 0i32;
+        let mut fuel_part2 = 0i32;
         for cur_position in &positions {
-            fuel += (target_position - cur_position).abs();
+            let diff = (target_position - cur_position).abs();
+            fuel_part1 += diff;
+            for d in 1..=diff {
+                fuel_part2 += d;
+            }
         }
-        match cheapest_fuel {
-            Some(f) => {
-                if fuel < f {
-                    cheapest_fuel = Some(fuel);
-                }
-            }
-            None => {
-                cheapest_fuel = Some(fuel);
-            }
+        if target_position == 0 {
+            cheapest_fuel_part1 = fuel_part1;
+            cheapest_fuel_part2 = fuel_part2;
+        } else {
+            cheapest_fuel_part1 = cmp::min(cheapest_fuel_part1, fuel_part1);
+            cheapest_fuel_part2 = cmp::min(cheapest_fuel_part2, fuel_part2);
         }
     }
 
-    println!("cheapest: {:?} fuel", cheapest_fuel.unwrap());
+    println!("cheapest part 1: {:?} fuel", cheapest_fuel_part1);
+    println!("cheapest part 2: {:?} fuel", cheapest_fuel_part2);
+    assert_eq!(344535, cheapest_fuel_part1);
+    assert_eq!(95581659, cheapest_fuel_part2);
 
     let elapsed_time = now.elapsed();
     println!("done in {} microseconds", elapsed_time.as_micros());
