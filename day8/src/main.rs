d9a3e70a165c4385a5a36d328c1a526dfde4068f
Mark Janssen <mark@praseodym.net>
2021-12-08 23:41:03 +0100

    Day 8: part two

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,35 +1,117 @@
+use std::collections::HashMap;
 use std::time::Instant;
 
 fn main() {
     let now = Instant::now();
 
     let input = include_str!("../input").trim_end().split('\n');
-    let mut digits = [0u32; 10];
+
+    let mut part1 = 0u32;
+    let mut part2 = 0u32;
 
     for line in input {
         let mut s = line.split('|');
-        let signals = s.next().unwrap();
-        let outputs = s.next().unwrap().trim().split(' ');
 
-        for output in outputs {
-            println!("output: {:?}", output);
-            let digit = match output.len() {
-                2 => Some(1),
-                4 => Some(4),
-                3 => Some(7),
-                7 => Some(8),
-                _ => None,
-            };
-            if let Some(d) = digit {
-                digits[d] += 1;
+        let signals_raw = s.next().unwrap().trim();
+
+        let mut signals_sorted = signals_raw.split(' ').collect::<Vec<&str>>();
+        signals_sorted.sort_by_key(|a| a.len());
+
+        let mut signals_occurences: HashMap<char, u8> = HashMap::new();
+        signals_raw.chars().filter(|&c| c != ' ').for_each(|c| {
+            signals_occurences
+                .entry(c)
+                .and_modify(|n| *n += 1)
+                .or_insert(1);
+        });
+
+        let mut mapping: HashMap<char, char> = HashMap::new();
+        for (c, n) in signals_occurences {
+            match n {
+                4 => {
+                    mapping.insert(c, 'e');
+                }
+                6 => {
+                    mapping.insert(c, 'b');
+                }
+                7 => {
+                    // d and g occurs 7 times - d is in 4 (len 4), g is not
+                    match signals_sorted[2].chars().find(|&p| p == c) {
+                        Some(_) => {
+                            mapping.insert(c, 'd');
+                        }
+                        None => {
+                            mapping.insert(c, 'g');
+                        }
+                    }
+                }
+                8 => {
+                    // a and c occur 8 times - a is not in 1 (len 2), c is
+                    match signals_sorted[0].chars().find(|&p| p == c) {
+                        Some(_) => {
+                            mapping.insert(c, 'c');
+                        }
+                        None => {
+                            mapping.insert(c, 'a');
+                        }
+                    }
+                }
+                9 => {
+                    mapping.insert(c, 'f');
+                }
+                _ => {
+                    panic!("unexpected: {} {}", c, n);
+                }
             }
         }
+
+        let outputs = s.next().unwrap().trim().split(' ');
+
+        let mut digits = "".to_string();
+        for output in outputs {
+            let mut mapped = output
+                .chars()
+                .map(|c| mapping.get(&c).unwrap())
+                .collect::<Vec<&char>>();
+            mapped.sort();
+            let new = mapped.into_iter().collect::<String>();
+
+            digits += match new.as_str() {
+                "abcefg" => "0",
+                "cf" => {
+                    part1 += 1;
+                    "1"
+                }
+                "acdeg" => "2",
+                "acdfg" => "3",
+                "bcdf" => {
+                    part1 += 1;
+                    "4"
+                }
+                "abdfg" => "5",
+                "abdefg" => "6",
+                "acf" => {
+                    part1 += 1;
+                    "7"
+                }
+                "abcdefg" => {
+                    part1 += 1;
+                    "8"
+                }
+                "abcdfg" => "9",
+                _ => {
+                    panic!("unexpected: {}", new);
+                }
+            }
+        }
+        part2 += digits.parse::<u32>().unwrap();
     }
 
-    println!(
-        "part one: {}",
-        digits[1] + digits[4] + digits[7] + digits[8]
-    );
+    println!("part one: {}", part1);
+    println!("part two: {}", part2);
+
+    assert_eq!(part1, 416);
+    assert_eq!(part2, 1043697);
 
     let elapsed_time = now.elapsed();
     println!("done in {} microseconds", elapsed_time.as_micros());
