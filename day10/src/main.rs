687d9f9a6bd413cf8bc324d10cb1b9ddfd49ae04
Mark Janssen <mark@praseodym.net>
2021-12-10 11:05:59 +0100

    Day 10: part two

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::VecDeque;
 use std::time::Instant;
 
 fn main() {
@@ -7,8 +6,9 @@ fn main() {
     let input = include_str!("../input").trim_end().split('\n');
 
     let mut part1 = 0u32;
+    let mut part2_scores: Vec<u64> = Vec::new();
 
-    for line in input {
+    'outer: for line in input {
         let mut s: Vec<char> = Vec::new();
         for char in line.chars() {
             match char {
@@ -19,35 +19,57 @@ fn main() {
                 ')' => {
                     if s.pop().unwrap() != '(' {
                         part1 += 3;
-                        break;
+                        continue 'outer;
                     }
                 }
                 ']' => {
                     if s.pop().unwrap() != '[' {
                         part1 += 57;
-                        break;
+                        continue 'outer;
                     }
                 }
                 '}' => {
                     if s.pop().unwrap() != '{' {
                         part1 += 1197;
-                        break;
+                        continue 'outer;
                     }
                 }
                 '>' => {
                     if s.pop().unwrap() != '<' {
                         part1 += 25137;
-                        break;
+                        continue 'outer;
                     }
                 }
                 _ => {
-                    println!("invalid character {}", char)
+                    panic!("invalid character {}", char);
                 }
             }
         }
+        let mut part2_score = 0u64;
+        while !s.is_empty() {
+            let char = s.pop().unwrap();
+            part2_score *= 5;
+            part2_score += match char {
+                '(' => 1,
+                '[' => 2,
+                '{' => 3,
+                '<' => 4,
+                _ => {
+                    panic!("invalid character {}", char);
+                }
+            };
+        }
+        part2_scores.push(part2_score);
     }
 
+    part2_scores.sort_unstable();
+    let &part2 = part2_scores.get(part2_scores.len() / 2).unwrap();
+
     println!("part 1: {}", part1);
+    println!("part 2: {}", part2);
+
+    assert_eq!(part1, 166191);
+    assert_eq!(part2, 1152088313);
 
     let elapsed_time = now.elapsed();
     println!("done in {} microseconds", elapsed_time.as_micros())
