6061111effa45a4d6572d915422ec7b612c91826
Mark Janssen <mark@praseodym.net>
2021-12-12 19:49:17 +0100

    Day 12: set up tests

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,15 +1,21 @@
+#![feature(test)]
 #![feature(type_alias_impl_trait)]
 
-use std::time::Instant;
+extern crate test;
 
 type Vertex = &'static str;
 type Edge = (Vertex, Vertex);
 type EdgeIterator<'a> = impl Iterator<Item = Edge>;
 
 fn main() {
-    let now = Instant::now();
+    let (paths_part1, paths_part2) = run(include_str!("../input"));
 
-    let input = include_str!("../input").trim_end().split('\n');
+    println!("paths part 1: {}", paths_part1);
+    println!("paths part 2: {}", paths_part2);
+}
+
+fn run(input: &'static str) -> (u16, u32) {
+    let input = input.trim_end().split('\n');
     let edges: Vec<Edge> = input
         .map(|l| l.split('-'))
         .map(|mut s| (s.next().unwrap(), s.next().unwrap()))
@@ -17,15 +23,7 @@ fn main() {
 
     let paths_part1 = dfs_part1(&edges, &"start", &[]);
     let paths_part2 = dfs_part2(&edges, &"start", &[], false);
-
-    println!("paths part 1: {}", paths_part1);
-    println!("paths part 2: {}", paths_part2);
-
-    assert_eq!(paths_part1, 5958);
-    assert_eq!(paths_part2, 150426);
-
-    let elapsed_time = now.elapsed();
-    println!("done in {} microseconds", elapsed_time.as_micros())
+    (paths_part1, paths_part2)
 }
 
 fn edges_from_vertex<'a>(edges: &'a [Edge], vertex: &'a Vertex) -> EdgeIterator<'a> {
@@ -81,4 +79,45 @@ fn dfs_part2<'a>(
         paths += dfs_part2(edges, &v, &visited, small_cave_once || small_cave_twice);
     }
     paths
+}
+
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+
+    use super::*;
+
+    #[test]
+    fn test_input_test1() {
+        let (paths_part1, paths_part2) = run(include_str!("../input-test1"));
+        assert_eq!(paths_part1, 10);
+        assert_eq!(paths_part2, 36);
+    }
+
+    #[test]
+    fn test_input_test2() {
+        let (paths_part1, paths_part2) = run(include_str!("../input-test2"));
+        assert_eq!(paths_part1, 19);
+        assert_eq!(paths_part2, 103);
+    }
+
+    #[test]
+    fn test_input_test3() {
+        let (paths_part1, paths_part2) = run(include_str!("../input-test3"));
+        assert_eq!(paths_part1, 226);
+        assert_eq!(paths_part2, 3509);
+    }
+
+    #[test]
+    fn test_input_own() {
+        let (paths_part1, paths_part2) = run(include_str!("../input"));
+        assert_eq!(paths_part1, 5958);
+        assert_eq!(paths_part2, 150426);
+    }
+
+    #[bench]
+    fn bench(b: &mut Bencher) {
+        let input = include_str!("../input");
+        b.iter(|| run(input));
+    }
 }
