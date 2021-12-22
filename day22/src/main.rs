71bc79579d9ed3be61ad103dc5cbcb2f57d7cb8d
Mark Janssen <mark@praseodym.net>
2021-12-22 18:18:04 +0100

    Day 22: part two
    test tests::bench ... bench:  16,370,776 ns/iter (+/- 513,038)

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -3,30 +3,44 @@
 
 extern crate test;
 
-use std::ops::Range;
-
-#[derive(Default, Debug, Copy, Clone, Hash, Eq, PartialEq)]
+#[derive(Debug, Copy, Clone)]
 struct Step {
     action: bool,
-    x1: isize,
-    x2: isize,
-    y1: isize,
-    y2: isize,
-    z1: isize,
-    z2: isize,
+    cuboid: Cuboid,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Cuboid {
+    x: (isize, isize),
+    y: (isize, isize),
+    z: (isize, isize),
 }
 
 fn main() {
-    let (part1_answer, _part2_answer) = run(include_str!("../input"));
+    let (part1_answer, part2_answer) = run(include_str!("../input"));
     println!("part 1 answer: {}", part1_answer);
-    // println!("part 2 answer: {}", part2_answer);
+    println!("part 2 answer: {}", part2_answer);
 }
 
 fn run(input: &'static str) -> (usize, usize) {
     let steps = parse_input(input);
-    let part1_answer = part1_run(&steps);
-    let part2_answer = 0;
-    (part1_answer, part2_answer)
+
+    let mut cuboids: Vec<Cuboid> = Vec::new();
+    for step in steps {
+        let mut new_cuboids: Vec<Cuboid> = Vec::new();
+        for other in cuboids {
+            new_cuboids.append(&mut other.intersect(&step.cuboid));
+        }
+        if step.action {
+            new_cuboids.push(step.cuboid);
+        }
+        cuboids = new_cuboids;
+    }
+
+    (
+        cuboids.iter().map(|c| c.volume_part1()).sum(),
+        cuboids.iter().map(|c| c.volume_part2()).sum(),
+    )
 }
 
 fn parse_input(input: &'static str) -> Vec<Step> {
@@ -51,56 +65,111 @@ fn parse_line(line: &'static str) -> Step {
     let mut z = s.next().unwrap().strip_prefix("z=").unwrap().split("..");
     Step {
         action,
-        x1: x.next().unwrap().parse().unwrap(),
-        x2: x.next().unwrap().parse().unwrap(),
-        y1: y.next().unwrap().parse().unwrap(),
-        y2: y.next().unwrap().parse().unwrap(),
-        z1: z.next().unwrap().parse().unwrap(),
-        z2: z.next().unwrap().parse().unwrap(),
+        cuboid: Cuboid {
+            x: (
+                x.next().unwrap().parse().unwrap(),
+                x.next().unwrap().parse().unwrap(),
+            ),
+            y: (
+                y.next().unwrap().parse().unwrap(),
+                y.next().unwrap().parse().unwrap(),
+            ),
+            z: (
+                z.next().unwrap().parse().unwrap(),
+                z.next().unwrap().parse().unwrap(),
+            ),
+        },
     }
 }
 
-fn part1_run(steps: &[Step]) -> usize {
-    let mut state = vec![vec![vec![false; 101]; 101]; 101];
-    for step in steps {
-        for z in part1_range(step.z1, step.z2) {
-            for y in part1_range(step.y1, step.y2) {
-                for x in part1_range(step.x1, step.x2) {
-                    state[z][y][x] = step.action;
-                }
+impl Cuboid {
+    fn intersect(mut self, other: &Cuboid) -> Vec<Cuboid> {
+        let mut ret = Vec::new();
+        if (self.x.0 <= other.x.1 && self.x.1 >= other.x.0)
+            && (self.y.0 <= other.y.1 && self.y.1 >= other.y.0)
+            && (self.z.0 <= other.z.1 && self.z.1 >= other.z.0)
+        {
+            if self.x.0 < other.x.0 {
+                ret.push(Cuboid {
+                    x: (self.x.0, other.x.0 - 1),
+                    y: self.y,
+                    z: self.z,
+                });
+                self.x.0 = other.x.0;
             }
-        }
-    }
-    let mut ret = 0;
-    for z in state {
-        for y in z {
-            for x in y {
-                ret += x as usize;
+            if self.x.1 > other.x.1 {
+                ret.push(Cuboid {
+                    x: (other.x.1 + 1, self.x.1),
+                    y: self.y,
+                    z: self.z,
+                });
+                self.x.1 = other.x.1;
             }
+            if self.y.0 < other.y.0 {
+                ret.push(Cuboid {
+                    x: self.x,
+                    y: (self.y.0, other.y.0 - 1),
+                    z: self.z,
+                });
+                self.y.0 = other.y.0;
+            }
+            if self.y.1 > other.y.1 {
+                ret.push(Cuboid {
+                    x: self.x,
+                    y: (other.y.1 + 1, self.y.1),
+                    z: self.z,
+                });
+                self.y.1 = other.y.1;
+            }
+            if self.z.0 < other.z.0 {
+                ret.push(Cuboid {
+                    x: self.x,
+                    y: self.y,
+                    z: (self.z.0, other.z.0 - 1),
+                });
+                self.z.0 = other.z.0;
+            }
+            if self.z.1 > other.z.1 {
+                ret.push(Cuboid {
+                    x: self.x,
+                    y: self.y,
+                    z: (other.z.1 + 1, self.z.1),
+                });
+                self.z.1 = other.z.1;
+            }
+        } else {
+            ret.push(self);
         }
+        ret
     }
-    ret
+
+    fn volume_part1(&self) -> usize {
+        diff_part1(self.x) * diff_part1(self.y) * diff_part1(self.z)
+    }
+
+    fn volume_part2(&self) -> usize {
+        (self.x.1 - self.x.0 + 1) as usize
+            * (self.y.1 - self.y.0 + 1) as usize
+            * (self.z.1 - self.z.0 + 1) as usize
+    }
 }
 
-fn part1_range(a: isize, b: isize) -> Range<usize> {
+fn diff_part1(s: (isize, isize)) -> usize {
+    let (mut a, mut b) = s;
     if (a < -50 && b < -50) || (a > 50 && b > 50) {
-        return 0..0;
+        return 0;
     }
-    let mut a = a + 50;
-    let mut b = b + 50;
-    if a < 0 {
-        a = 0;
-    } else if a > 100 {
-        a = 100;
+    if a < -50 {
+        a = -50;
+    } else if a > 50 {
+        a = 50;
     }
-    if b < 0 {
-        b = 0;
-    } else if b > 100 {
-        b = 100;
-    } else {
-        b += 1;
+    if b < -50 {
+        b = -50;
+    } else if b > 50 {
+        b = 50;
     }
-    a as usize..b as usize
+    (b - a + 1) as usize
 }
 
 #[cfg(test)]
@@ -117,23 +186,28 @@ mod tests {
 
     #[test]
     fn test_input_example1() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example1"));
+        let (part1_answer, _) = run(include_str!("../input-example1"));
         assert_eq!(part1_answer, 39);
-        // assert_eq!(part2_answer, 0);
     }
 
     #[test]
     fn test_input_example2() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input-example2"));
+        let (part1_answer, _) = run(include_str!("../input-example2"));
         assert_eq!(part1_answer, 590784);
-        // assert_eq!(part2_answer, 0);
+    }
+
+    #[test]
+    fn test_input_example3() {
+        let (part1_answer, part2_answer) = run(include_str!("../input-example3"));
+        assert_eq!(part1_answer, 474140);
+        assert_eq!(part2_answer, 2758514936282235);
     }
 
     #[test]
     fn test_input_own() {
-        let (part1_answer, _part2_answer) = run(include_str!("../input"));
+        let (part1_answer, part2_answer) = run(include_str!("../input"));
         assert_eq!(part1_answer, 607657);
-        // assert_eq!(part2_answer, 0);
+        assert_eq!(part2_answer, 1187742789778677);
     }
 
     #[bench]
